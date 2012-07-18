2623b6447bc6a9e3e8ba41353b2f96e8f2ca1170
Rastio Hodul <rhodul@gmail.com>
2012-07-18 15:37:26 -0700

    Trying to install subsite with drush.

--- site.pp
+++ site.pp
@@ -9,4 +9,22 @@ define site ($sitename = $title, $targetdir, $mysqlpwd, $contribmodules = undef)
 		target => "${targetdir}/",
 		require => Class["drupal"],
 	}
+
+	# create site
+	exec { "installsite${sitename}":
+		path => ['/tmp/drush/', '/usr/bin/php', '/usr/bin/', '/bin/'],
+		command => "drush -l ${sitename} -y site-install standard --account-name=admin --account-pass=admin --db-su=root --db-su-pw=${mysqlpwd} --db-url=mysql://${sitename}:`openssl rand -base64 16`@localhost/${sitename}",
+		cwd => "/var/www/",
+		creates => '/var/www/sites/${sitename}/settings.php',
+    timeout => 0,
+    require => File["sitelink${sitename}"],
+	}
+	file { "makefileswritable${sitename}":
+		ensure => directory,
+		path => "/var/www/sites/${sitename}/files",
+		mode => 0777,
+		recurse => true,
+    require => Exec["installsite${sitename}"],
+	}
+	
 }
