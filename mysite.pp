d8d1ab49b357cba142b2b9f30a92f9ad9b601f84
Rastio Hodul <rhodul@gmail.com>
2012-07-19 14:24:08 -0700

    Simplified

---
+++ mysite.pp
@@ -0,0 +1,9 @@
+# this is the only file you need to edit to configure
+# your own site
+site {"site1":
+  custommodulesdir => "/home/rhodul/mysite1/modules",
+  customthemesdir => "/home/rhodul/mysite1/themes",
+  #customlibsdir => "/home/rhodul/mysite1/libs",
+  # this is not set the latest release of Drupal is installed
+  #drupalversion => '7.14',
+}
--- site.pp
+++ site.pp
@@ -1,30 +1,76 @@
-define site ($sitename = $title, $targetdir, $mysqlpwd, $contribmodules = undef) {
-	include drush
-	include drupal
-
-	# create a link in Drupal to target
-	file { "sitelink${sitename}":
-		ensure => link,
-		path => "/var/www/sites/${sitename}/",
-		target => "${targetdir}/",
-		require => Class["drupal"],
+define site ($sitename = $title, $custommodulesdir, $customthemesdir, $customlibsdir = undef, $drupalversion = undef) {
+	# do we have a version?
+	$drupal = 'drupal'
+	if $drupalversion != undef {
+		$drupal = "drupal-${drupalversion}"
 	}
 
+	# create a subdir for the site
+	file { "sitedir${sitename}":
+		ensure => directory,
+		path => "/var/www/${sitename}/",
+	}
+
+	# ensure drupal core
+	exec { "getdrupal${sitename}":
+		path => ['/usr/bin/php', '/usr/bin/', '/bin/'],
+		command => "drush dl ${drupal} --destination=/tmp/ --drupal-project-rename=drupal",
+		creates => '/tmp/drupal/',
+    timeout => 0,
+	}
+	file { "movedrupal${sitename}":
+		ensure => present,
+		path => "/var/www/${sitename}/",
+		source => '/tmp/drupal/',
+		recurse => true,
+    require => [File["sitedir${sitename}"], Exec["getdrupal${sitename}"]],
+	}
+	file { "removedrupaltmp{sitename}":
+		ensure => absent,
+		force => true,
+		path => "/tmp/drupal/",
+    require => File["movedrupal"],
+	}
+	
 	# create site
 	exec { "installsite${sitename}":
-		path => ['/tmp/drush/', '/usr/bin/php', '/usr/bin/', '/bin/'],
-		command => "drush -l ${sitename} -y site-install standard --account-name=admin --account-pass=admin --db-su=root --db-su-pw=${mysqlpwd} --db-url=mysql://${sitename}:`openssl rand -base64 16`@localhost/${sitename}",
-		cwd => "/var/www/",
-		creates => '/var/www/sites/${sitename}/settings.php',
+		path => ['/usr/bin/php', '/usr/bin/', '/bin/'],
+		#TODO: use `read "What's your MySql root pwd:`
+		#     ?run interactive/verbose??
+		command => "read -p 'Type MySql root password:' MYSQL_PWD; drush -y site-install standard --account-name=admin --account-pass=admin --db-su=root --db-su-pw=$MYSQL_PWD --db-url=mysql://${sitename}:`openssl rand -base64 16`@localhost/${sitename}",
+		cwd => "/var/www/${sitename}/",
+		creates => '/var/www/${sitename}/sites/default/settings.php',
     timeout => 0,
-    require => File["sitelink${sitename}"],
+    require => File["movedrupal${sitename}"],
 	}
 	file { "makefileswritable${sitename}":
 		ensure => directory,
-		path => "/var/www/sites/${sitename}/files",
+		path => "/var/www/${sitename}/sites/default/files",
 		mode => 0777,
 		recurse => true,
     require => Exec["installsite${sitename}"],
+	}
+
+	# create a link(s) in Drupal to target
+	file { "moduleslink${sitename}":
+		ensure => link,
+		path => "/var/www/${sitename}/sites/all/modules/${sitename}",
+		target => "${custommodulesdir}/",
+    require => Exec["installsite${sitename}"],
+	}
+	file { "themeslink${sitename}":
+		ensure => link,
+		path => "/var/www/${sitename}/sites/all/themes/${sitename}",
+		target => "${customthemesdir}/",
+    require => Exec["installsite${sitename}"],
+	}
+	if $customlibsdir != undef {
+		file { "libslink${sitename}":
+			ensure => link,
+			path => "/var/www/${sitename}/sites/all/libs/",
+			target => "${customlibsdir}/",
+			require => Exec["installsite${sitename}"],
+		}
 	}
 	
 }
